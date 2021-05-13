615b606d29d7f604b3074e12bf75ac96e70dff98
Tiktak228 <49941257+Tiktak228@users.noreply.github.com>
2021-05-13 17:06:46 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,5 +12,48 @@ $('.slider2').slick({
 	variableWidth: true,
 	slidesToScroll:2,
 	speed:800,
+	waitForAnimate:false,
+	responsive:[
+	{
+		breakpoint:1294,
+		settings: {
+		arrows : false,
+		}
+	}
+	]
+});
+});
+$(document).ready(function(){
+$('.slider3').slick({
+	dots: false,
+	variableWidth: true,
+	slidesToShow:1,
+	arrows : false,
+	asNavFor: ".slider4",
+	speed:800,
+	waitForAnimate:false,
+	//touchThreshold:30,
+});
+});
+$(document).ready(function(){
+$('.slider4').slick({
+	dots: true,
+	variableWidth: true,
+	slidesToShow:3,
+	slidesToScroll:1,
+	arrows : true,
+	asNavFor: ".slider3",
+	speed:800,
+	waitForAnimate:false,
+	waitForAnimate:false,
+	responsive:[
+	{
+		breakpoint:1294,
+		settings: {
+		arrows : false,
+		}
+	}
+	]
+	//touchThreshold:10,
 });
 });
